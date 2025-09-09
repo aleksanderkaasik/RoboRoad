39dcee6c884aca6c72f2a566085f841c9df142a6
alexkaasik <aleks.kaasik@outlook.com>
2025-09-09 20:44:47 +0000

    Now having, list of node showing Nodes names. And Viewing simple info about the data

--- resources/js/react/components/MainContent.jsx
+++ resources/js/react/components/MainContent.jsx
@@ -3,13 +3,14 @@ import React, { useState } from 'react';
 import TabContent from './tabContent.jsx';
 import Tabs from './tabs.jsx';
 
-const MainContent = () => {
+const MainContent = ({data, selectedNode }) => {
     const [activeTab, setActiveTab] = useState(0);
+    const extractedNodes = data?.data || [];
 
     return (
         <div className="main-content">
             <Tabs activeTab={activeTab} onTabClick={setActiveTab} />
-            <TabContent activeTab={activeTab} />
+            <TabContent activeTab={activeTab} data={extractedNodes} selectedNode={selectedNode} />
         </div>
     );
 };
--- resources/js/react/components/tabContent.jsx
+++ resources/js/react/components/tabContent.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Sidebar from "./sidebar.jsx";
 import MainContent from "./MainContent.jsx";
 
-const TabContent = ({ activeTab }) => {
+const TabContent = ({ activeTab, data, selectedNode }) => {
     const [systemInfo, setSystemInfo] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
@@ -11,8 +11,18 @@ const TabContent = ({ activeTab }) => {
         <div className="content-body">
             {activeTab === 0 && (
                 <div>
-                    <h2>Tab 1 Content</h2>
-                    <p>This is the content of Tab 1.</p>
+                    {selectedNode ? (
+                        <p>
+                            Selected Node ID: {selectedNode.NodeId}
+                            <br/>
+                            Selected Node Name: {selectedNode.NodeName}
+                            <br/>
+                            Selected Node Address: {selectedNode.NodeAddress}
+                        </p>
+
+                    ) : (
+                        <p>No node selected.</p>
+                    )}
                 </div>
             )}
 
