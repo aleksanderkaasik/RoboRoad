71f9413c40af2c951f5bd7745dd5a0fe9d221201
alexkaasik <aleks.kaasik@outlook.com>
2025-09-11 02:42:54 +0000

    Adding search bar to find specific node

--- resources/js/react/components/sidebar.jsx
+++ resources/js/react/components/sidebar.jsx
@@ -2,13 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 import ListNodes from './listNodes.jsx';
 import Logo from './logo.jsx';
+import NodeSearch from './NodeSearch.jsx';
 
 const Sidebar = ({ data, onNodeSelect, selectedNode }) => {
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const filteredNodes = data.filter(node =>
+        node.NodeName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        node.NodeAddress.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
     return (
         <div className="Sidebar">
             <Logo />
+
+            <NodeSearch
+                searchQuery={searchQuery}
+                setSearchQuery={setSearchQuery}
+            />
+
             <ListNodes
-                nodes={data}
+                nodes={filteredNodes}
                 onNodeSelect={onNodeSelect}
                 selectedNode={selectedNode}
             />
